8c41f1cded9cfcc21d6f2f7c31b7a29e2b91575d
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-06 15:34:59 -0300

    Assets de la app

---
+++ 01-musicfy/src/assets/index.ts
@@ -0,0 +1,9 @@
+export * from "./jpg/background-auth.jpg";
+export * from "./jpg/banner-home.jpg";
+
+export * from "./png/default-user.png";
+export * from "./png/logo-green.png";
+export * from "./png/logo-name-green.png";
+export * from "./png/logo-name-white.png";
+export * from "./png/logo-white.png";
+export * from "./png/no-image.png";
