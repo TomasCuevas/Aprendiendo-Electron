18565b2ff31975e7646274945f38aaee7e14813c
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-05 21:54:38 -0300

    Sistema de variables de SASS

--- 01-musicfy/src/main.tsx
+++ 01-musicfy/src/main.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+//* component *//
 import { App } from "./App";
+
+//* styles *//
+import "@/scss/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
