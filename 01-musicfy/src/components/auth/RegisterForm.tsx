0381d68b76bfd41ce7b279bb73270b503ece9b68
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-06 16:24:30 -0300

    Creando el formulario de registro

--- 01-musicfy/src/components/auth/RegisterForm.tsx
+++ 01-musicfy/src/components/auth/RegisterForm.tsx
@@ -1,4 +1,7 @@
-import { Button } from "semantic-ui-react";
+import { Button, Form, Icon } from "semantic-ui-react";
+
+//* styles *//
+import "@/components/auth/registerForm.scss";
 
 //* interface *//
 interface Props {
@@ -8,15 +11,43 @@ interface Props {
 
 export const RegisterForm: React.FC<Props> = ({ goBack, goLogin }) => {
   return (
-    <div style={{ backgroundColor: "#000" }}>
-      <h1>RegisterForm</h1>
+    <div className="register__form">
+      <h1>Empieza a escuchar con una cuenta de Musicfy gratis</h1>
 
-      <Button primary onClick={goLogin}>
-        Login
-      </Button>
-      <Button secondary onClick={goBack}>
-        Atras
-      </Button>
+      <Form>
+        <Form.Input
+          placeholder="Correo electronico"
+          icon="mail outline"
+          type="text"
+          error={true}
+        />
+        <Form.Input
+          placeholder="Contraseña"
+          icon={
+            <Icon
+              name="eye"
+              link
+              onClick={() => console.log("show password")}
+            />
+          }
+          type="password"
+        />
+        <Form.Input
+          placeholder="¿Cómo deberíamos llamarte?"
+          icon="user cicle outline"
+          type="text"
+        />
+        <Form.Button type="submit" primary fluid>
+          Continuar
+        </Form.Button>
+      </Form>
+
+      <div className="register__form-options">
+        <p onClick={goBack}>Volver</p>
+        <p>
+          ¿Ya tienes Musicfy? <span onClick={goLogin}>Iniciar sesión</span>
+        </p>
+      </div>
     </div>
   );
 };
