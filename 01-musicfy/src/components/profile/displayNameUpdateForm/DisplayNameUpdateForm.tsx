67bd3e23bea7b51cd537384169f6d8b30c2bb701
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-10 03:29:16 -0300

    Controlando y validando formulario

--- 01-musicfy/src/components/profile/displayNameUpdateForm/DisplayNameUpdateForm.tsx
+++ 01-musicfy/src/components/profile/displayNameUpdateForm/DisplayNameUpdateForm.tsx
@@ -1,4 +1,19 @@
+import { useUserStore } from "@/store";
+import { useFormik } from "formik";
 import { Form } from "semantic-ui-react";
+import * as Yup from "yup";
+
+//* yup validations *//
+const validationSchema = () => {
+  return Yup.object({
+    displayName: Yup.string().required(),
+  });
+};
+
+//* form values *//
+const initialValues = (value: string) => ({
+  displayName: value,
+});
 
 //* interface *//
 interface Props {
@@ -6,10 +21,28 @@ interface Props {
 }
 
 export const DisplayNameUpdateForm: React.FC<Props> = ({ onClose }) => {
+  const { getMe } = useUserStore();
+
+  const formik = useFormik({
+    initialValues: initialValues(getMe()!.displayName || ""),
+    validationSchema: validationSchema,
+    validateOnChange: false,
+    onSubmit: async (formValues) => {
+      console.log(formValues);
+    },
+  });
+
   return (
-    <Form>
-      <Form.Input name="displayName" placeholder="Nombre y apellido" />
-      <Form.Button type="submit" primary fluid>
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="displayName"
+        placeholder="Nombre y apellido"
+        onChange={formik.handleChange}
+        value={formik.values.displayName}
+        error={formik.errors.displayName ? true : false}
+        focus
+      />
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
         Actualizar nombre
       </Form.Button>
     </Form>
