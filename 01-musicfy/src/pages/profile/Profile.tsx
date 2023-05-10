f05c950f7ce5848b45a3bb50bd2a587dcc0eb83e
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-09 23:02:23 -0300

    Estructura de los ajustes

--- 01-musicfy/src/pages/profile/Profile.tsx
+++ 01-musicfy/src/pages/profile/Profile.tsx
@@ -1,7 +1,38 @@
+//* styles *//
+import { Button } from "semantic-ui-react";
+import "./profile.scss";
+
+//* store *//
+import { useAuthStore } from "@/store";
+
 export const Profile: React.FC = () => {
+  const { getMe } = useAuthStore();
+  const user = getMe();
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <h1>Configuración</h1>
+      <div className="profile__block">
+        <div>
+          <p>AVATAR</p>
+          <span>{user?.displayName}</span>
+        </div>
+        <Button onClick={() => console.log("Cambiar displayName")}>
+          Actualizar
+        </Button>
+      </div>
+
+      <div className="profile__block">
+        <span>Email: {user?.email}</span>
+        <Button onClick={() => console.log("Cambiar Email")}>Actualizar</Button>
+      </div>
+
+      <div className="profile__block">
+        <span>Contraseña: *********</span>
+        <Button onClick={() => console.log("Cambiar contraseña")}>
+          Actualizar
+        </Button>
+      </div>
     </div>
   );
 };
