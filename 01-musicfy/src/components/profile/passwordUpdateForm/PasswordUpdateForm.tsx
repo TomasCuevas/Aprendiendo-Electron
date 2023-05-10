6369fee5a8accbf7b29f6203b3d85e03254dbb3f
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-10 05:23:07 -0300

    Controlando y validando formulario

--- 01-musicfy/src/components/profile/passwordUpdateForm/PasswordUpdateForm.tsx
+++ 01-musicfy/src/components/profile/passwordUpdateForm/PasswordUpdateForm.tsx
@@ -1,6 +1,27 @@
+import { useFormik } from "formik";
 import { useState } from "react";
 import { Form } from "semantic-ui-react";
+import * as Yup from "yup";
 
+//* yup validations *//
+const validationSchema = () => {
+  return Yup.object({
+    password: Yup.string().required(),
+    newPassword: Yup.string().required(),
+    repeatNewPassword: Yup.string()
+      .required()
+      .oneOf([Yup.ref("newPassword")]),
+  });
+};
+
+//* form values *//
+const initialValues = () => ({
+  password: "",
+  newPassword: "",
+  repeatNewPassword: "",
+});
+
+//* interface *//
 interface Props {
   onClose(): void;
 }
@@ -8,10 +29,21 @@ interface Props {
 export const PasswordUpdateForm: React.FC<Props> = ({ onClose }) => {
   const [showPassword, setShowPassword] = useState(false);
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema,
+    validateOnChange: false,
+    onSubmit: async (formValues) => {
+      console.log(formValues);
+    },
+  });
+
   return (
-    <Form>
+    <Form onSubmit={formik.handleSubmit}>
       <Form.Input
         type={showPassword ? "text" : "password"}
+        value={formik.values.password}
+        onChange={formik.handleChange}
         name="password"
         placeholder="Contraseña actual"
         icon={{
@@ -19,9 +51,12 @@ export const PasswordUpdateForm: React.FC<Props> = ({ onClose }) => {
           link: true,
           onClick: () => setShowPassword((prev) => !prev),
         }}
+        error={formik.errors.password ? true : false}
       />
       <Form.Input
         type={showPassword ? "text" : "password"}
+        value={formik.values.newPassword}
+        onChange={formik.handleChange}
         name="newPassword"
         placeholder="Nueva Contraseña"
         icon={{
@@ -29,18 +64,22 @@ export const PasswordUpdateForm: React.FC<Props> = ({ onClose }) => {
           link: true,
           onClick: () => setShowPassword((prev) => !prev),
         }}
+        error={formik.errors.newPassword ? true : false}
       />
       <Form.Input
         type={showPassword ? "text" : "password"}
-        name="repeatPassword"
+        value={formik.values.repeatNewPassword}
+        onChange={formik.handleChange}
+        name="repeatNewPassword"
         placeholder="Repetir la nueva Contraseña"
         icon={{
           name: showPassword ? "eye slash" : "eye",
           link: true,
           onClick: () => setShowPassword((prev) => !prev),
         }}
+        error={formik.errors.repeatNewPassword ? true : false}
       />
-      <Form.Button type="submit" primary fluid>
+      <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
         Actualizar contraseña
       </Form.Button>
     </Form>
