31fb342e8d40e439a477a40850269731d0422d87
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-12 21:34:03 -0300

    Obteniendo los datos de un artista

--- 01-musicfy/src/pages/artist/Artist.tsx
+++ 01-musicfy/src/pages/artist/Artist.tsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+//* service *//
+import { getOneArtist } from "@/services";
+
+//* interface *//
+import { IArtist } from "@/interfaces";
+
 export const Artist: React.FC = () => {
+  const [artist, setArtist] = useState<IArtist>();
+  const { id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getOneArtist(id!);
+        setArtist(response);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [id]);
+
   return (
     <div>
       <h1>Artist</h1>
--- 01-musicfy/src/services/artist/artist.services.ts
+++ 01-musicfy/src/services/artist/artist.services.ts
@@ -1,11 +1,11 @@
-import { setDoc, doc, collection, getDocs } from "firebase/firestore";
+import { setDoc, doc, collection, getDocs, getDoc } from "firebase/firestore";
 import { v4 as uuid } from "uuid";
 
 //* database *//
 import { database } from "@/utils";
 
 //* collection name
-const COLLECTION_ARTIST_NAME = "artists";
+const ARTISTS_COLLECTION = "artists";
 
 //* interfaces *//
 import { IArtist } from "@/interfaces";
@@ -17,7 +17,7 @@ export const createArtist = async (image: string, name: string) => {
     const createdAt = new Date().getTime();
     const data = { id: idArtist, image, name, createdAt };
 
-    const docRef = doc(database, COLLECTION_ARTIST_NAME, idArtist);
+    const docRef = doc(database, ARTISTS_COLLECTION, idArtist);
     await setDoc(docRef, data);
   } catch (error) {
     throw error;
@@ -27,10 +27,22 @@ export const createArtist = async (image: string, name: string) => {
 //! get all artists [service]
 export const getAllArtists = async () => {
   try {
-    const docRef = collection(database, COLLECTION_ARTIST_NAME);
+    const docRef = collection(database, ARTISTS_COLLECTION);
     const snapshot = await getDocs(docRef);
 
     return snapshot.docs.map((doc) => doc.data()) as IArtist[];
+  } catch (error) {
+    throw error;
+  }
+};
+
+//! get one arstist [service]
+export const getOneArtist = async (id: string) => {
+  try {
+    const docRef = doc(database, ARTISTS_COLLECTION, id);
+    const snapshot = await getDoc(docRef);
+
+    return snapshot.data() as IArtist;
   } catch (error) {
     throw error;
   }
